ac0f084e1df79c9452f55a94a47bf0cb40db9ac3
dududuzhengjun <dududuzhengjun@gmail.com>
2023-02-20 17:07:55 +0800

    wan_web完成申办一套流程

--- src/test/java/com/qa/pagetest/WanWeb.java
+++ src/test/java/com/qa/pagetest/WanWeb.java
@@ -2,6 +2,8 @@ package com.qa.pagetest;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -31,7 +33,7 @@ public class WanWeb{
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         //进入wanweb登录页
         driver.get("https://200000001-zaixian.sdndc.cn/account/login");
-        System.out.println("成功打开wanweb首页");
+        System.out.println("成功打开wan_web首页");
 
         //浏览器最大化
         driver.manage().window().maximize();
@@ -51,23 +53,132 @@ public class WanWeb{
 
         //点击进入申办
         driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[2]/div/div/div/div/div/div/div/div[1]/div/div/div/div/div[2]/div[1]/a")).click();
-        Thread.sleep(3000);
+        Thread.sleep(3500);
         //点击右上角“开始申办”按钮
         driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[1]/div[2]/div/div/div[2]/span[2]/button")).click();
-        Thread.sleep(1000);
+        Thread.sleep(1500);
         //点击第一个申办场景“公众买糖场景”
         driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[2]/div/div/div/div[2]/div/div/div/div[1]/div/div")).click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         //点击右上角开始申办按钮
         driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[1]/div[2]/div/div/div[2]/span[2]/span/button")).click();
+        Thread.sleep(1000);
         //点击选择大厅
         driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div[2]/div[2]/div[4]/div[4]/div/h3")).click();
-        Thread.sleep(1000);
-        //不选择团队直接点击申办按钮
+        Thread.sleep(2000);
+//        //不选择团队直接点击申办按钮
+//        driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div[2]/div[3]/button")).click();
+        //点击选择金咖社业务团队
+        driver.findElement(By.xpath("//div[contains(text(),'金咖社业务团队')]")).click();
+        Thread.sleep(1500);
         driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div[2]/div[3]/button")).click();
+
+
+        Thread.sleep(8000);
+        System.out.println("登记业务成功，待填写资料后申办");
+
+        //填写基本信息
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[2]/div/div/div/div[3]/div/div/div[1]/div/div/div[1]")).click();
+        Thread.sleep(1000);
+        //用途
+        driver.findElement(By.xpath("//*[@id=\"wanPurposeId\"]/div/div/div[2]")).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//div[contains(text(),'买房子')]")).click();
+        //几套房子
+        driver.findElement(By.xpath("//*[@id=\"field_4863\"]")).sendKeys("3");
+        //腾讯
+        driver.findElement(By.xpath("//*[@id=\"field_4036\"]")).sendKeys("腾讯成都大厦A座");
+        //称呼
+        driver.findElement(By.xpath("//*[@id=\"field_4860\"]")).sendKeys("杜征骏");
+        //手机号
+        driver.findElement(By.xpath("//*[@id=\"field_4861\"]")).sendKeys("13002840927");
+        //职业
+        driver.findElement(By.xpath("//*[@id=\"field_4862\"]")).sendKeys("面点师");
+        //手机号整数
+        driver.findElement(By.xpath("//*[@id=\"field_4864\"]")).sendKeys("17");
+
+
+
+        //点击确定提交基本信息
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        System.out.println("基本信息填写完成");
+        Thread.sleep(4000);
+
+        //设置当事人
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[2]/div/div/div/div[3]/div/div/div[2]/div/div/div[1]/div[2]/div/span")).click();
+        //添加申请人 杜征骏
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[2]/div/div/div/div[2]/div/div[1]/div/table/tbody/tr/td[3]/span[2]/button[1]")).click();
+        driver.findElement(By.xpath("//*[@id=\"idType\"]/div/div/div[1]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[contains(text(),'身份证人像面')]")).click();
+        driver.findElement(By.xpath("//*[@id=\"idNumber\"]/div/div/ul/li/div/input")).sendKeys("510921199409275117");
+        driver.findElement(By.xpath("//*[@id=\"name\"]")).sendKeys("杜征骏");
+        driver.findElement(By.xpath("//*[@id=\"mobile\"]")).sendKeys("13002840927");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Thread.sleep(1500);
+//        //添加公众角色 自己
+//        driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[2]/div/div/div/div[3]/div/div[1]/div/table/tbody/tr/td[3]/span[2]/button[2]")).click();
+        //添加公众角色 田欣
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[2]/div/div/div/div[3]/div/div[1]/div/table/tbody/tr/td[3]/span[2]/button[1]")).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@id=\"idType\"]/div/div/div[1]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[contains(text(),'居民身份证正面')]")).click();
+        driver.findElement(By.xpath("//*[@id=\"idNumber\"]/div/div/ul/li/div/input")).sendKeys("51300219930912544X");
+        driver.findElement(By.xpath("//*[@id=\"name\"]")).sendKeys("田欣");
+        driver.findElement(By.xpath("//*[@id=\"mobile\"]")).sendKeys("19182261929");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[1]/div[2]/div/div/div[2]/span[2]/button")).click();
+        System.out.println("添加当事人成功");
         Thread.sleep(2000);
 
+        //上传材料
 
+
+        //选择模板
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[2]/div/div/div/div[3]/div/div/div[4]/div/div/div[1]/div[2]/div/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[2]/div/div/div/div/span/div/div/div/div[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[2]/div/div/div/div/span/div/div/div/div[2]/div/input")).sendKeys("委托书");
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[1]/div[2]/div/div/div[2]/span[1]/div/h4")).click();
+        driver.findElement(By.xpath("/html/body/div[2]/div/section/section/main/div[1]/div[1]/div[2]/div/div/div[2]/span[3]/div/button[2]")).click();
+        System.out.println("选择模板成功");
+        Thread.sleep(2000);
+
+        //预览文书
+
+
+        //提交申办
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[2]/div/div/div/div[3]/div/button")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div[4]/div/div[2]/div/div[2]/div[3]/div/button[2]")).click();
+        Thread.sleep(3000);
+        System.out.println("申办业务成功");
+        //返回申办详情
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[4]/button")).click();
+        Thread.sleep(3000);
+
+        //作废申办业务
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div[1]/div[2]/div/div/div/div[3]/div/h4/div/button")).click();
+        driver.findElement(By.xpath("/html/body/div[4]/div/div/div/div[2]/div/div/div[2]/button[2]")).click();
+        Thread.sleep(2000);
+        System.out.println("作废业务成功");
+
+
+
+
+
+
+        //封装select对象
+//        Select select = new Select(driver.findElement(By.xpath("//*[@id=\"idType\"]/div/div/div[1]")));
+//        for (WebElement e : select.getOptions())
+//            e.click();
+//        select.selectByVisibleText("身份证人面像");
+//        driver.findElement(By.xpath("//*[@id=\"283407b7-a991-4659-e3a5-549c8a11d816\"]/ul/li[2]/div")).click();
 
 
 
